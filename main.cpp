529c0ad10477f4037c368af942b34fb35dcaa2bd
rwkarren <rob@karren.com>
2021-04-21 08:22:49 -1000

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "pico/stdlib.h"
+#include "mcp2515.h"
+#include "MS5837.h"
+
+void init() {
+    
+}
+
+int main()
+{
+    const uint LED_PIN = 25;
+    const uint RADIO_BUTTON_PIN = 2;
+    gpio_init(LED_PIN);
+    gpio_init(RADIO_BUTTON_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_set_dir(RADIO_BUTTON_PIN, GPIO_IN);
+
+    while (true) {
+        bool led_on = gpio_get(RADIO_BUTTON_PIN);
+
+        if (led_on) {
+            gpio_put(LED_PIN, 1);
+        } else {
+            gpio_put(LED_PIN, 0);
+        }
+
+    }
+}
+
